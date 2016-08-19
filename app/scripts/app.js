0c684830ca164f336d63da05407ea28e06d127b2
toco94 <amejia@cetys.edu.mx>
2016-08-18 21:39:57 -0700

    trying

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name succesfulstudentApp
+ * @description
+ * # succesfulstudentApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('succesfulstudentApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/tareas', {
+        templateUrl: 'views/tareas.html',
+        controller: 'TareasCtrl',
+        controllerAs: 'tareas'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/tareas.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name succesfulstudentApp.controller:TareasCtrl
+ * @description
+ * # TareasCtrl
+ * Controller of the succesfulstudentApp
+ */
+angular.module('succesfulstudentApp')
+  .controller('TareasCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
